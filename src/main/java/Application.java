d0e2b60d425711562fd17e9e03eeb4ff6a70187b
Rafa Velazquez <er.velazquez@hotmail.com>
2019-10-10 10:01:47 -0500

    Commit inicial

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hola amigos");
+        System.out.println(3*4);
+        System.out.println("suma: " + 2+8);
+    }
+}
